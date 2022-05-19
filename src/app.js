3fbc8321529ae06156efb94958e24722ee94df13
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-05-19 11:34:13 -0400

    Updated Forecast with API

--- src/app.js
+++ src/app.js
@@ -23,7 +23,8 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -50,6 +51,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "b6520355a84f46a27e6fe4523cdc2546";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -73,6 +81,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -143,4 +153,4 @@ let kmhWindLink = document.querySelector("#kmh-link");
 kmhWindLink.addEventListener("click", displayMhConversion);
 
 search("New York");
-displayForecast();
+
